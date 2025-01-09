10a2a290b440cb653194afc914f556ed90877c40
olenaliubynetska <olena.liuby@gmail.com>
2025-01-09 18:28:34 +0000

    feat: configured User model

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
-    "wishlist"
+    "wishlist",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# User
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+	fieldsets = (
+		(None, {"fields": ("email", "password")}),
+		("Personal info", {"fields": ("first_name", "last_name")}),
+		(
+			"Permissions",
+			{"fields": ("is_active", "is_staff", "is_superuser", "user_permissions")},
+		),
+	)
+	add_fieldsets = (
+		(
+			None,
+			{
+				"classes": ("wide",),
+				"fields": (
+					"email",
+					"password1",
+					"password2",
+					"is_staff",
+					"is_superuser",
+				),
+			},
+		),
+	)
+	list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser")
+	search_fields = ("email", "first_name", "last_name")
+	ordering = ("email",)
